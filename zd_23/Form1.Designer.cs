fc73344667bcd4c9d63720b5e815fde689006813
zemerovaanna <zemerovagmail.com>
2025-06-03 16:39:09 +0500

    zd_2

---
+++ zd_23/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace zd_23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zd_2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.price = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.магазинToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.плейлистToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zd_3 = new System.Windows.Forms.Panel();
+            this.zd_2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zd_2
+            // 
+            this.zd_2.Controls.Add(this.label3);
+            this.zd_2.Controls.Add(this.numericUpDown2);
+            this.zd_2.Controls.Add(this.label2);
+            this.zd_2.Controls.Add(this.textBox1);
+            this.zd_2.Controls.Add(this.label1);
+            this.zd_2.Controls.Add(this.numericUpDown1);
+            this.zd_2.Controls.Add(this.button1);
+            this.zd_2.Controls.Add(this.price);
+            this.zd_2.Controls.Add(this.dataGridView1);
+            this.zd_2.Location = new System.Drawing.Point(196, 12);
+            this.zd_2.Name = "zd_2";
+            this.zd_2.Size = new System.Drawing.Size(583, 426);
+            this.zd_2.TabIndex = 0;
+            //this.zd_2.Paint += new System.Windows.Forms.PaintEventHandler(this.zd_2_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Количество";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(13, 114);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(209, 20);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Стоимость";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Название товара";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 75);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(209, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.addBut_Click);
+            // 
+            // price
+            // 
+            this.price.AutoSize = true;
+            this.price.Location = new System.Drawing.Point(370, 12);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(59, 13);
+            this.price.TabIndex = 1;
+            this.price.Text = "Прибыль: ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.price_col,
+            this.count});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 198);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(555, 211);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SellProd);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.SellProd);
+            // 
+            // name
+            // 
+            this.name.HeaderText = "имя";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // price_col
+            // 
+            this.price_col.HeaderText = "цена";
+            this.price_col.Name = "price_col";
+            this.price_col.ReadOnly = true;
+            // 
+            // count
+            // 
+            this.count.HeaderText = "количество";
+            this.count.Name = "count";
+            this.count.ReadOnly = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.магазинToolStripMenuItem,
+            this.плейлистToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
+            this.toolStripMenuItem1.Text = "Меню";
+            // 
+            // магазинToolStripMenuItem
+            // 
+            this.магазинToolStripMenuItem.Name = "магазинToolStripMenuItem";
+            this.магазинToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.магазинToolStripMenuItem.Text = "Магазин";
+            this.магазинToolStripMenuItem.Click += new System.EventHandler(this.магазинToolStripMenuItem_Click);
+            // 
+            // плейлистToolStripMenuItem
+            // 
+            this.плейлистToolStripMenuItem.Name = "плейлистToolStripMenuItem";
+            this.плейлистToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.плейлистToolStripMenuItem.Text = "Плейлист";
+            this.плейлистToolStripMenuItem.Click += new System.EventHandler(this.плейлистToolStripMenuItem_Click);
+            // 
+            // zd_3
+            // 
+            this.zd_3.Location = new System.Drawing.Point(155, 20);
+            this.zd_3.Name = "zd_3";
+            this.zd_3.Size = new System.Drawing.Size(608, 400);
+            this.zd_3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.zd_3);
+            this.Controls.Add(this.zd_2);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.zd_2.ResumeLayout(false);
+            this.zd_2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel zd_2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem магазинToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem плейлистToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label price;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price_col;
+        private System.Windows.Forms.DataGridViewTextBoxColumn count;
+        private System.Windows.Forms.Panel zd_3;
+    }
+}
+
---
+++ zd_23/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zd_23
+{
+    public partial class Form1 : Form
+    {
+        Shop shop;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // загрузка магазина
+        private void магазинToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            zd_3.Visible = false;
+            zd_2.Visible = true;
+            shop = new Shop();
+        }
+
+        // загрузка формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            zd_2.Visible = false;
+            zd_3.Visible = false;
+        }
+
+        // метод защищенного ввода данных о товаре
+        private void addBut_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+            numericUpDown1.BackColor = Color.White;
+            numericUpDown2.BackColor = Color.White;
+
+            // защита имени
+            if (textBox1.Text == "")
+            {
+                textBox1.BackColor = Color.Tomato;
+                return;
+            }
+
+            // защита цены
+            if (numericUpDown1.Value == 0)
+            {
+                numericUpDown1.BackColor = Color.Tomato;
+                return;
+            }
+
+            // защита количества
+            if (numericUpDown2.Value == 0)
+            {
+                numericUpDown2.BackColor = Color.Tomato;
+                return;
+            }
+
+            if (shop.FindByName(textBox1.Text) != null)
+            {
+                MessageBox.Show("Такой товар уже есть");
+                return;
+            }
+
+            dataGridView1.Rows.Add(new string[] { textBox1.Text, numericUpDown1.Value.ToString(), numericUpDown2.Value.ToString() });
+            shop.CreateProduct(textBox1.Text, numericUpDown1.Value, (int)numericUpDown2.Value);
+
+            textBox1.Text = "";
+            numericUpDown1.Value = 0;
+            numericUpDown2.Value = 0;
+        }
+
+        // метод покупки товара с обновлением таблицы
+        private void SellProd(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells[0].RowIndex == -1 || dataGridView1.Rows.Count == 1) return;
+
+            if (shop.Sell(dataGridView1[0, dataGridView1.CurrentCell.RowIndex].Value.ToString()))
+            {
+                dataGridView1.Rows.Clear();
+
+                // загрузка буфера удаления
+                List<Product> delet = new List<Product>();
+
+                // обход словаря товаров для обновления
+                foreach (var pr in shop.DictProd())
+                {
+                    if (pr.Value > 0) dataGridView1.Rows.Add(new string[] { pr.Key.Name, pr.Key.Price.ToString(), pr.Value.ToString() });
+                    else delet.Add(pr.Key);
+                }
+
+                // удаление по буферу
+                foreach (var pr in delet)
+                {
+                    shop.DictProd().Remove(pr);
+                }
+
+                price.Text = $"Прибыль: {shop.Budget()}";
+            }
+            else
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        // переход на плейлист
+        private void плейлистToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            zd_2.Visible = false;
+            zd_3.Visible = true;
+        }
+
+    }
+}
